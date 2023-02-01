0c9912156f4c30e622ca238626653b33d110ad50
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-02-01 19:37:09 +0300

    app useState olusturuldu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({})
+  useEffect (() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=WDVLfQss0FOfcv6WelZtNLXR1qEQv5Hv8INlW7Bh')
+  .then(function (response) {
+    setData({
+      copyright: response.data.copyright,
+      title: response.data.title,
+      date: response.data.date,
+      explanation: response.data.explanation,
+      url: response.data.url,
+    });
+  })
+  },[])
+  const { copyright, date, explanation, title, url } = data;
   return (
     <div className="App">
       <p>
